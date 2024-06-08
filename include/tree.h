9d5cd5526ff8772ec42a3601304a9c56bbe38ac3
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-06-08 16:14:41 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,27 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+
+class Tree {
+ public:
+    struct Node {
+        char data;
+        std::vector<Node*> children;
+
+        Node(char data) : data(data) {}
+    };
+
+ private:
+    Node* root;
+    std::vector<std::vector<char>> permutations;
+
+    void buildTree(Node* node, const std::vector<char>& elements);
+    void getPermutations(Node* node, std::vector<char>& currentPermutation);
+
+ public:
+    Tree(const std::vector<char>& elements);
+    std::vector<char> getPerm(int n);
+};
 
 #endif  // INCLUDE_TREE_H_
