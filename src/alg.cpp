7a50599b0762c4615bb5df276bae793813ecc7b7
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-06-08 16:53:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,11 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    std::vector<std::string> allPermutations = tree.getAllPermutations();
+    if (n > 0 && n <= allPermutations.size()) {
+        std::string perm = allPermutations[n - 1];
+        return std::vector<char>(perm.begin(), perm.end());
+    } else {
+        return std::vector<char>();
+    }
 }
